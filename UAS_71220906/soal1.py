4d04f6f0c0f71f05bd262516dc9e57f356b927c5
rezon123 <118422752+rezon123@users.noreply.github.com>
2023-01-03 12:32:09 +0700

    Add files via upload

---
+++ UAS_71220906/soal1.py
@@ -0,0 +1,18 @@
+print ("****** kredit keaktifan mahasiswa ******")
+print ("student activities credit")
+print ("1. menambahkan kegiatan")
+print ("2. menampilkan jumlah poin")
+print ("3. keluar")
+
+pilihan = int(input("silahkan masukkan pilihan anda: "))
+if pilihan == 1:
+    HMTI2022 = int(input("masukkan nama kegiatan: HMTI 2022 "))
+print ("nama kegiatan: HMTI 2022")
+print ("tanggal kegiatan: tahun 2022-2023")
+print ("- prestasi")
+print ("- organisasi")
+print ("- kepantiaan")
+print ("- rekognisi")
+print ("masukkan kategori kegiatan : organisasi")
+print ("kegiatan berhasil ditambhkan.")
+
